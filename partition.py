7516f8bb2be1f6a816fbb69b2eb0832b52df44b1
Şiyar Yıkmış <siyar93@gmail.com>
2019-12-07 11:34:37 +0100

    Update partition.py

--- partition.py
+++ partition.py
@@ -1,12 +1,11 @@
 import argparse
+import bcolors
 import csv
 import glob
 import os
 import shutil
 import sys
 from timeit import default_timer as timer
-
-import bcolors
 
 prediction_results = 'day_night_prediction.csv'
 
@@ -36,33 +35,42 @@ def check_dirs(path):
         print("Given Directory don't exists! Creating directory...")
         os.mkdir(path)
 
-
-os.mkdir('results')
+path= "results"
+if not os.path.exists(path) and not os.path.isdir(path):
+    os.mkdir('results')
 day_path = 'results/day'
 night_path = 'results/night'
 parser = argparse.ArgumentParser()
 parser.add_argument('--path', type=str, help='path to img dir to partition')
 
 FLAGS = parser.parse_args()
+
 check_dirs(day_path)
 check_dirs(night_path)
 
 files = os.listdir(FLAGS.path)
 files_list = [f for f in files if not f.startswith(".")]
+files_len = len(files_list)
 i = 0
 start = timer()
 for file in files_list:
-    prediction = prediction_dict[file]
+    try:
+        prediction = prediction_dict[file]
+    except KeyError:
+        print(bcolors.WARN + "Key for " + file + " not found" + bcolors.END)
+        files_len = files_len - 1
+        continue
+
     if prediction == "night":
         shutil.copy(os.path.join(FLAGS.path, file), night_path)
     elif prediction == "day":
         shutil.copy(os.path.join(FLAGS.path, file), day_path)
 
-    if i % 125 == 0:
+    if i % round(files_len*0.05) == 0:
         print(bcolors.WAITMSG + '[INFO] Partitioned %.2f percent of all test files'
               % (i / len(files_list) * 100) + bcolors.ENDC)
 
     i += 1
 end = timer()
-print(bcolors.OKMSG + "[INFO] The partitioning of " + str(len(files_list)) + " images took " + str(
-    round(end - start, 2)) + " seconds")
+print(bcolors.BLUE + "[INFO] The partitioning of " + str(len(files_list)) + " images took " + str(
+    round(end - start, 2)) + " seconds"+ bcolors.END)
